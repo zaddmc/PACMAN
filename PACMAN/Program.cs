ae868210bad446d9c4a805fffeac2ed09b48524f
Zadd ZC <davidbk927@gmail.com>
2024-02-22 18:08:39 +0100

    Add project files.

---
+++ PACMAN/Program.cs
@@ -0,0 +1,202 @@
+﻿using System.Diagnostics;
+
+namespace PACMAN;
+
+internal class Program {
+    static Tile[][]? Tiles { get; set; }
+    static TimeSpan WantedFrameTime = TimeSpan.FromSeconds(0.2);
+    static int chargeTime = 15; // how many frames the charged state will remain after taking effect
+    static void Main(string[] args) {
+
+        Console.WindowHeight = 40;
+        Console.CursorVisible = false;
+
+        SetupBoard();
+        GameLoop();
+
+
+
+    }
+    static int Charge = 0;
+    static int Score = 0;
+    static void GameLoop() {
+
+        bool running = true;
+
+
+        while (running) {
+
+
+            UpdateFrame();
+            WriteStatus();
+
+            TimeDelay();
+        }
+
+    }
+    static void WriteStatus() {
+        int width = Tiles[0].Length + 5;
+        int height = 3;
+
+        Console.SetCursorPosition(width, height++);
+        Console.Write("Score: " + Score);
+        Console.SetCursorPosition(width, height++);
+        Console.Write("IsCharged: " + (Charge > 0).ToString() + "    ");
+        Console.SetCursorPosition(width, height++);
+        Console.Write("FoodLeft: " + FoodLeft + "    ");
+
+    }
+    static ConsoleKeyInfo Key { get; set; }
+    static Direction Direction { get; set; }
+    public static Position Player { get; set; }
+    public static int FoodLeft { get; set; }
+    static void UpdateFrame(bool called = false) {
+        if (Console.KeyAvailable) {
+            Key = Console.ReadKey(true);
+        }
+
+        Direction previousDirection = Direction;
+
+        switch (Key.KeyChar) {
+            case 'w': Direction = Direction.North; break;
+            case 'a': Direction = Direction.West; break;
+            case 's': Direction = Direction.South; break;
+            case 'd': Direction = Direction.East; break;
+            default: break;
+        }
+
+        Position newPosTile = new(Player.I, Player.J);
+        switch (Direction) {
+            case Direction.North: newPosTile.I += -1; break;
+            case Direction.East: newPosTile.J += 1; break;
+            case Direction.South: newPosTile.I += 1; break;
+            case Direction.West: newPosTile.J += -1; break;
+            default: break;
+        }
+
+        bool change = false;
+        switch (Tiles[newPosTile.I][newPosTile.J].Type) {
+            case Tile.TileType.Wall:
+                break;
+            case Tile.TileType.Teleport:
+                break;
+            case Tile.TileType.Charge:
+                change = true; Charge = chargeTime; Score += 50; FoodLeft--;
+                break;
+            case Tile.TileType.Food:
+                change = true; Score += 10; FoodLeft--;
+                break;
+            case Tile.TileType.Empty:
+                change = true;
+                break;
+            case Tile.TileType.Start:
+                change = true;
+                break;
+            default:
+                break;
+        }
+        if (change) { 
+            UpdateTile(Player);
+            Player = newPosTile; 
+            UpdateTile(Player, true);
+        }
+
+        if (Charge > 0) Charge--;
+        if (FoodLeft == 0) SetupBoard();
+    }
+    static void UpdateTile(Position position, bool player = false) {
+        Tile tile = Tiles[position.I][position.J];
+        tile.Type = Tile.TileType.Empty;
+        tile.Art = ' ';
+        Console.SetCursorPosition(position.J, position.I);
+        
+        Console.Write(player ? "Ö" : tile.Art);
+    }
+
+
+    static void SetupBoard() {
+        string[] boardInfo = Assets.DrawMainBoard() ?? throw new ArgumentException("board call failed");
+
+        Tiles = new Tile[boardInfo.Length][];
+        for (int i = 0; i < Tiles.Length; i++) {
+            Tiles[i] = new Tile[boardInfo[i].Length];
+            for (int j = 0; j < Tiles[i].Length; j++) {
+                Tiles[i][j] = new Tile(boardInfo[i][j], new Position(i, j));
+            }
+        }
+
+        Console.SetCursorPosition(0, 0);
+        for (int i = 0; i < Tiles.Length; i++) {
+            for (int j = 0; j < Tiles[i].Length; j++) {
+                Tile tile = Tiles[i][j];
+                switch (tile.Type) {
+                    case Tile.TileType.Wall:
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        break;
+                    case Tile.TileType.Teleport:
+                        break;
+                    case Tile.TileType.Charge:
+                    case Tile.TileType.Food:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case Tile.TileType.Empty:
+                        break;
+                    default:
+                        break;
+                }
+                Console.Write(Tiles[i][j].Art);
+            }
+            Console.WriteLine();
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+
+
+    static private DateTime timeStamp = DateTime.Now;
+    static void TimeDelay() {
+        TimeSpan waitFor = WantedFrameTime - (DateTime.Now - timeStamp);
+        if (waitFor > TimeSpan.Zero) Thread.Sleep(waitFor);
+        timeStamp = DateTime.Now;
+    }
+}
+public class Tile {
+    public enum TileType {
+        Wall,
+        Teleport,
+        Charge,
+        Food,
+        Empty,
+        Start,
+    }
+    public TileType Type { get; set; }
+    public char Art { get;  set; }
+    public Tile(char seed, Position position) {
+        Art = seed;
+        switch (seed) {
+            case '█': Type = TileType.Wall; break;
+            case '*': Type = TileType.Food; Program.FoodLeft++; break;
+            case '+': Type = TileType.Charge; Program.FoodLeft++; break;
+            case 't': Type = TileType.Teleport; Art = ' '; break;
+            case ' ': Type = TileType.Empty; break;
+            case 's': Type = TileType.Start; Art = 'Ö'; Program.Player = position; break;
+            default: Type = TileType.Empty; Debug.WriteLine("unexpected empty call"); break;
+        }
+    }
+    
+}
+public class Position {
+    public int I { get; set; }
+    public int J { get; set; }
+
+    public Position(int i, int j) {
+        I = i;
+        J = j;
+    }
+}
+public enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
